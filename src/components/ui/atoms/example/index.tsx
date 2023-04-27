9da058ff9abf71d3aaf5f6d2a24c095424315ba7
bl0ckp1n9 <blockp1n9@gmail.com>
2023-04-27 17:00:38 +0900

    feat: add acordion navigation

--- src/components/ui/atoms/example/index.tsx
+++ src/components/ui/atoms/example/index.tsx
@@ -1,19 +1,13 @@
 import clsx from 'clsx'
-import styled from 'styled-components'
-import tw from 'twin.macro'
 
 interface ExampleProps {
   className?: string
 }
 
-const Header = styled.h1`
-  ${tw`p-5 text-center text-slate-100`}
-`
-
 export default function Index({ className, ...rest }: ExampleProps) {
   return (
     <div className={clsx('bg-slate-500', className)} {...rest}>
-      <Header>Header</Header>
+      <h1>Heading</h1>
     </div>
   )
 }
